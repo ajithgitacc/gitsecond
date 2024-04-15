e968fd66f1486ee9f832b9b1705698bb6440a7e7
ajithgitacc <ajithkumarmoorthy11@gmail.com>
2024-04-15 18:58:32 +0530

    task

---
+++ src/test/java/org/task/Sample.java
@@ -0,0 +1,14 @@
+package org.task;
+
+public class Sample {
+	private void Day1() {
+		System.out.println("Day1");
+	}
+	public static void main(String[] args) {
+		Sample sample= new Sample();
+		sample.Day1();
+	}
+
+}
+
+
